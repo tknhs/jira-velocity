ec4890bbcfae2aa450e45050deea3f70a97ccc9c
takanohashi <takanohashi@sansan.com>
2023-09-11 00:36:12 +0900

    update libs

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-from pydantic import BaseSettings, Field
-
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 class JiraConfig(BaseSettings):
-    server_uri: str = Field(..., env="SERVER_URI")
-    user: str = Field(..., env="AUTH_USERNAME")
-    api_token: str = Field(..., env="AUTH_API_TOKEN")
-    project_id: str = Field(..., env="PROJECT_ID")
+    server_uri: str = Field(..., alias="SERVER_URI")
+    user: str = Field(..., alias="AUTH_USERNAME")
+    api_token: str = Field(..., alias="AUTH_API_TOKEN")
+    project_id: str = Field(..., alias="PROJECT_ID")
